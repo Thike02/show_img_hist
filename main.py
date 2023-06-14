244de2e68c4099a86b34e5651635456dae2e903c
Thike <cmt14136@ict.nitech.ac.jp>
2023-06-14 18:07:02 +0900

    change image

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+img_url = 'c:/Users/takuy/OneDrive - NITech/画像/kadai1_3.png'
 
 im = imread(img_url)
 
